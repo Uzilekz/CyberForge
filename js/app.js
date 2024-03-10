386cd51e388b58f3235973a183f3b0e2fc4522ac
Gerardo P.A <p.a.gerardo.software.engineer@gmail.com>
2024-03-10 01:04:33 -0300

    3ra PreEntrega

--- js/app.js
+++ js/app.js
@@ -1,271 +1,87 @@
-//PRODUCTOS
+// Definición de productos y precios
 const productos = [
-    //PLACA MADRE
-    {
-        id: "placaMadre-01",
-        titulo: "Motherboard Asus Prime A320m-k",
-        imagen: "../img/products/placaMadre/MotherboardAsusPrimeA320m-k.webp",
-        categoria: {
-            id: "placasMadre",
-            nombre: "Placa Madre",
-        },
-        precio: 150,
-    },
-    {
-        id: "placaMadre-02",
-        titulo: "Motherboard Asus Prime A520m-a Ii",
-        imagen: "../img/products/placaMadre/MotherboardAsusPrimeA520m-aIi.webp",
-        categoria: {
-            id: "placasMadre",
-            nombre: "Placa Madre",
-        },
-        precio: 200,
-    },
-    {
-        id: "placaMadre-03",
-        titulo: "Motherboard Am4 Gigabyte B450m Ds3h",
-        imagen: "../img/products/placaMadre/MotherboardAm4GigabyteB450mDs3h.webp",
-        categoria: {
-            id: "placasMadre",
-            nombre: "Placa Madre",
-        },
-        precio: 300,
-    },
-
-    //PROCESADOR
-    //INTEL
-    {
-        id: "procesadorIntel-01",
-        titulo: "Procesador Intel Core i3-10100F",
-        imagen: "../img/products/procesador/INTEL/ProcesadorIntelCorei3-10100F.webp",
-        categoria: {
-            id: "procesadores",
-            nombre: "Procesador",
-        },
-        precio: 80,
-    },
-    {
-        id: "procesadorIntel-02",
-        titulo: "Procesador Intel Core i5-10400",
-        imagen: "../img/products/procesador/INTEL/ProcesadorIntelCorei5-10400.webp",
-        categoria: {
-            id: "procesadores",
-            nombre: "Procesador",
-        },
-        precio: 145,
-    },
-    {
-        id: "procesadorIntel-03",
-        titulo: "Procesador Intel Core i7-10700",
-        imagen: "../img/products/procesador/INTEL/ProcesadorIntelCorei7-10700.webp",
-        categoria: {
-            id: "procesadores",
-            nombre: "Procesador",
-        },
-        precio: 250,
-    },
-    //AMD
-    {
-        id: "procesadorAMD-01",
-        titulo: "Procesador AMD Ryzen 3 3200G",
-        imagen: "../img/products/procesador/AMD/ProcesadorAMDRyzen33200G.webp",
-        categoria: {
-            id: "procesadores",
-            nombre: "Procesador",
-        },
-        precio: 110,
-    },
-    {
-        id: "procesadorAMD-02",
-        titulo: "Procesador AMD Ryzen5 5600X",
-        imagen: "../img/products/procesador/AMD/ProcesadorAMDRyzen55600X.webp",
-        categoria: {
-            id: "procesadores",
-            nombre: "Procesador",
-        },
-        precio: 175,
-    },
-    {
-        id: "procesadorAMD-03",
-        titulo: "Procesador AMD Ryzen 7 5700G",
-        imagen: "../img/products/procesador/AMD/ProcesadorAMDRyzen75700G.webp",
-        categoria: {
-            id: "procesadores",
-            nombre: "Procesador",
-        },
-        precio: 250,
-    },
-
-    //TARJETA GRAFICA
-    //NVIDIA
-    {
-        id: "tarjetaGraficaNvidia-01",
-        titulo: "Placa De Video Nvidia Gigabyte GeForce 10 Series GT1030 2GB",
-        imagen: "../img/products/tarjetaGrafica/NVIDIA/PlacaDeVideoNvidiaGigabyteGeForce10SeriesGT10302GB.webp",
-        categoria: {
-            id: "tarjetasGraficas",
-            nombre: "Tarjeta Grafica",
-        },
-        precio: 125,
-    },
-    {
-        id: "tarjetaGraficaNvidia-02",
-        titulo: "Placa De Video Nvidia Evga KO Gaming GeForce RTX 20 Series RTX2060 6GB",
-        imagen: "../img/products/tarjetaGrafica/NVIDIA/PlacaDeVideoNvidiaEvgaKOGamingGeForceRTX20SeriesRTX20606GB.webp",
-        categoria: {
-            id: "tarjetasGraficas",
-            nombre: "Tarjeta Grafica",
-        },
-        precio: 250,
-    },
-    {
-        id: "tarjetaGraficaNvidia-03",
-        titulo: "Placa De Video Nvidia GeForce RTX 30 Series RTX3090 24GB",
-        imagen: "../img/products/tarjetaGrafica/NVIDIA/PlacaDeVideoNvidiaGeForceRTX30SeriesRTX309024GB.webp",
-        categoria: {
-            id: "tarjetasGraficas",
-            nombre: "Tarjeta Grafica",
-        },
-        precio: 500,
-    },
-
-    //AMD
-    {
-        id: "tarjetaGraficaAMD-01",
-        titulo: "Placa De Video AMD Afox Radeon RX500 Series RX580 8GB",
-        imagen: "../img/products/tarjetaGrafica/AMD/PlacaDeVideoAMDAfoxRadeonRX500SeriesRX5808GB.webp",
-        categoria: {
-            id: "tarjetasGraficas",
-            nombre: "Tarjeta Grafica",
-        },
-        precio: 150,
-    },
-    {
-        id: "tarjetaGraficaAMD-02",
-        titulo: "Placa De Video AMD Radeon RX6700 Series RX6700XT 12GB",
-        imagen: "../img/products/tarjetaGrafica/AMD/PlacaDeVideoAMDSapphirePulseRadeonRX6700SeriesRX6700XT12GB.webp",
-        categoria: {
-            id: "tarjetasGraficas",
-            nombre: "Tarjeta Grafica",
-        },
-        precio: 300,
-    },
-    {
-        id: "tarjetaGraficaAMD-03",
-        titulo: "Placa De Video Radeon RX6900XT 16gb Founders Edition",
-        imagen: "../img/products/tarjetaGrafica/AMD/PlacaDeVideoRadeonRX6900XT16gbFoundersEdition.webp",
-        categoria: {
-            id: "tarjetasGraficas",
-            nombre: "Tarjeta Grafica",
-        },
-        precio: 500,
-    },
-
-    //MEMORIA RAM
-    {
-        id: "memoriaRAM-01",
-        titulo: "Memoria Kingston Fury Rgb 8gb Ddr4",
-        imagen: "../img/products/memoriaRam/MemoriaKingstonFuryRgb8gbDdr4.webp",
-        categoria: {
-            id: "memoriasRAM",
-            nombre: "Memoria RAM",
-        },
-        precio: 50,
-    },
-    {
-        id: "memoriaRAM-02",
-        titulo: "Memoria Kingston Fury 16gb Ddr4",
-        imagen: "../img/products/memoriaRam/MemoriaKingstonFury16gbDdr4.webp",
-        categoria: {
-            id: "memoriasRAM",
-            nombre: "Memoria RAM",
-        },
-        precio: 100,
-    },
-    {
-        id: "memoriaRAM-03",
-        titulo: "Memoria Kingston Fury Renegade Rgb 32gb Ddr4",
-        imagen: "../img/products/memoriaRam/MemoriaKingstonFuryRenegadeRgb32gbDdr4.webp",
-        categoria: {
-            id: "memoriasRAM",
-            nombre: "Memoria RAM",
-        },
-        precio: 175,
-    },
-
-    //ALMACENAMIENTO
-    //HDD
-    {
-        id: "almacenamientoHDD-01",
-        titulo: "Disco Duro Interno Seagate 1TB",
-        imagen: "../img/products/almacenamiento/HDD/DiscoDuroInternoSeagate1TB.webp",
-        categoria: {
-            id: "almacenamientos",
-            nombre: "Almacenamiento",
-        },
-        precio: 50,
-    },
-    {
-        id: "almacenamientoHDD-02",
-        titulo: "Disco Duro Interno Western Digital 2TB",
-        imagen: "../img/products/almacenamiento/HDD/DiscoDuroInternoWesternDigital2TB.webp",
-        categoria: {
-            id: "almacenamientos",
-            nombre: "Almacenamiento",
-        },
-        precio: 75,
-    },
-
-    //SSD
-    {
-        id: "almacenamientoSSD-01",
-        titulo: "Disco Solido Interno Kingston 500GB",
-        imagen: "../img/products/almacenamiento/SSD/DiscoSolidoInternoKingston500GB.webp",
-        categoria: {
-            id: "almacenamientos",
-            nombre: "Almacenamiento",
-        },
-        precio: 75,
-    },
-    {
-        id: "almacenamientoSSD-02",
-        titulo: "Disco Solido Interno Kingston 960GB",
-        imagen: "../img/products/almacenamiento/SSD/DiscoSolidoInternoKingston960GB.webp",
-        categoria: {
-            id: "almacenamientos",
-            nombre: "Almacenamiento",
-        },
-        precio: 125,
-    },
+    { nombre: "Procesador", precio: 50000 },
+    { nombre: "Tarjeta Gráfica", precio: 100000 },
+    { nombre: "Memoria RAM", precio: 20000 },
+    { nombre: "Disco Duro", precio: 15000 },
 ];
 
-const contenedorProductos = document.querySelector("#productsContainer");
-const botonesCategorias = document.querySelectorAll(".nav-menu__button");
+// Array para almacenar los productos seleccionados durante la compra
+const carrito = [];
 
-//Cargar productos en el HTML
-function cargarProductos() {
-    //Busca en la lista de objetos(productos), por cada "producto"(variable que se le da a cada uno de los objetos), crea un div para cada uno de ellos con la información siguiente.
-    productos.forEach((producto) => {
-        const div = document.createElement("div");
-        div.classList.add("product");
-        div.innerHTML = `
-        <img class="product__img" src="${producto.imagen}" alt="${producto.titulo}" />
-        <div class="product__description">
-            <h3 class="product__description__name">${producto.titulo}</h3>
-            <p class="product__description__price">$${producto.precio}</p>
-            <button class="product__description__add" id="${producto.id}">Agregar</button>
-        </div>
-        `;
-
-        contenedorProductos.append(div);
+// Función para mostrar la lista de productos disponibles
+function mostrarProductos() {
+    let listaProductos = "Ingrese el número del producto que desea comprar: \n";
+    productos.forEach(({ nombre, precio }, index) => {
+        listaProductos += `${index + 1}. ${nombre} - $${precio}\n`;
     });
+    return listaProductos;
 }
 
-cargarProductos();
+// Función para realizar una compra
+function comprar() {
+    const opcion = parseInt(prompt(mostrarProductos())) || 0;
+    const opcionValida = opcion >= 1 && opcion <= productos.length;
 
-//Cambiar entre categorias de productos
-botonesCategorias.foreach((boton) => {
-    boton.addEventListener("click", (e) => {
-        e.currentTarget.classList.add("active");
+    if (opcionValida) {
+        const cantidad =
+            parseInt(prompt("Ingrese la cantidad que desea comprar:")) || 0;
+        const cantidadValida = cantidad > 0;
+
+        if (cantidadValida) {
+            const productoSeleccionado = productos[opcion - 1];
+            const total = productoSeleccionado.precio * cantidad;
+
+            // Utilizando spread para agregar el producto al carrito
+            carrito.push({ ...productoSeleccionado, cantidad, total });
+
+            alert(
+                `Has agregado ${cantidad} ${productoSeleccionado.nombre} al carrito. Total: $${total}`
+            );
+        } else {
+            alert("La cantidad ingresada no es válida.");
+        }
+    } else {
+        alert(
+            "Opción no válida. Por favor, ingrese un número de producto válido."
+        );
+    }
+}
+
+// Función para mostrar los productos en el carrito
+function mostrarCarrito() {
+    let mensaje = "Productos en tu carrito:\n";
+    carrito.forEach(({ nombre, cantidad, total }) => {
+        mensaje += `${cantidad} ${nombre} - Total: $${total}\n`;
     });
-});
+    return mensaje;
+}
+
+// Bienvenida a la tienda
+const nombreUsuario = prompt("Ingrese su nombre:");
+
+if (nombreUsuario) {
+    alert(`¡Hola ${nombreUsuario}! Bienvenido a 'CyberForge'.`);
+
+    // Permitir al usuario comprar productos múltiples
+    let deseaComprarOtro = true;
+
+    while (deseaComprarOtro) {
+        // Ofrecer la opción de comprar
+        const deseaComprar = confirm;
+
+        if (deseaComprar) {
+            comprar();
+            deseaComprarOtro = confirm("¿Desea seguir comprando?");
+        } else {
+            deseaComprarOtro = false;
+        }
+    }
+
+    // Si el carrito no está vacío, mostrar el carrito y despedirse
+    if (carrito.length > 0) {
+        alert(mostrarCarrito());
+    }
+
+    alert("Gracias por visitar 'CyberForge'.");
+}
